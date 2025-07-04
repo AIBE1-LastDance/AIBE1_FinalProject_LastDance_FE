911299f5e72fd1e72b40fb97734f924f9f2b5a9d
hanjungho <thegamerhan@gmail.com>
2025-07-04 12:59:48 +0900

    fix: base_uri 수정

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,12 @@ export default defineConfig({
     // port: 3000,
       proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://api.woori-zip.lastdance.store',
         changeOrigin: true,
         secure: false
       },
       '/oauth2': {
-        target: 'http://localhost:8080',
+        target: 'https://api.woori-zip.lastdance.store',
         changeOrigin: true,
         secure: false
       },
